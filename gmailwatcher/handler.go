7b3f5092cbc5b1e6ea25a0d4046cb326ffcf0f31
spavett <872808+spavett@users.noreply.github.com>
2024-02-04 09:16:47 +0000

    Initial commit

---
+++ gmailwatcher/handler.go
@@ -0,0 +1,70 @@
+package function
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
+
+	user := "me"
+	watch, err := srv.Users.Watch(user, &gmail.WatchRequest{
+		LabelIds:  []string{"INBOX"},
+		TopicName: "projects/wordleboard-1705220199954/topics/wordle-mail-notifications",
+	}).Do()
+	if err != nil {
+		log.Fatalf("Unable to watch: %v", err)
+	}
+	fmt.Printf("Watch: %v\n", watch)
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from file: %v", err)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
